f9cc15fd70be8f3b619b824b716cbff5848018d2
WillHoll <willhollingsworth15@yahoo.com>
2019-10-14 17:32:02 -0600

    app-6 complete

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Todo from './component/Todo'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      taskList: []
+    }
+  }
+
+  inputHandler(input){
+    this.setState({input: input})
+  }
+
+  addTodo() {
+    this.setState({
+      taskList: [...this.state.taskList, this.state.input],
+      input: ''
+    })
+  }
+
   render() {
+    let arrDisplay = this.state.taskList.map((el, i) => <Todo key={i} task={el}/>);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My to-do List:</h1>
+        <input value={this.state.input} onChange={e => this.inputHandler(e.target.value)}/>
+        <button onClick={() => this.addTodo()}>Add</button>
+        <div>{arrDisplay}</div>
       </div>
     );
   }
