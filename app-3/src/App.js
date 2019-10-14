500d1562821a1c7969cb64e3e3f223bf8a4128e7
WillHoll <willhollingsworth15@yahoo.com>
2019-10-14 15:33:31 -0600

    app-3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      array: ['Oregano','Basil', 'Tumeric', 'Cumim', 'Sage', 'Thyme', 'Parsley', 'Paprika', 'Rosemery', 'Cilantro', 'Sasafrass', 'Bay leaves', 'Jasmine'],
+      filteredArray: []
+    }
+  }
+  
+
+  filterThrough(input) {
+    let stateArray = this.state.array;
+    let newArray = [];
+    for (let i = 0; i < stateArray.length; i++) {
+      if (stateArray[i].includes(input)) {
+        newArray.push(stateArray[i])
+      }
+    }
+    this.setState({filteredArray: newArray})
+  }
+
   render() {
+    let arrayDisplay = this.state.filteredArray.map((el, i) => <h2 key={i}>{el}</h2>)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(event) => this.filterThrough(event.target.value)}/>
+      <div>{arrayDisplay}</div>
       </div>
     );
   }
