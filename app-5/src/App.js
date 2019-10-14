a357decc5f601d73db66f523632995e9d8644abc
WillHoll <willhollingsworth15@yahoo.com>
2019-10-14 16:31:58 -0600

    app-5 complete

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Image from './component/Image'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image 
+          url={'https://picsum.photos/seed/plol/200/300'}
+        />
       </div>
     );
   }
---
+++ app-5/src/component/Image.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Image extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <img src={this.props.url} alt="stuff"/>
+        )
+    }
+}
